2cdeb037fc97276743bdda0347385ae745bd70a1
Alexander Sushkevich <alexinforest@gmail.com>
2019-10-14 17:34:03 +0300

    project folders refactoredd

--- producer/server.js
+++ producer/server.js
@@ -3,38 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const fs = require("fs");
-// const amqp = require("amqplib/callback_api");
 
 const { PORT } = process.env;
-
-// setTimeout(() => {
-//   amqp.connect(process.env.MESSAGE_QUEUE, function(error0, connection) {
-//     if (error0) {
-//       throw error0;
-//     }
-//     connection.createChannel(function(error1, channel) {
-//       if (error1) {
-//         throw error1;
-//       }
-
-//       const queue = "hello";
-//       const msg = "Hello Alex!";
-
-//       channel.assertQueue(queue, {
-//         durable: false
-//       });
-//       channel.sendToQueue(queue, Buffer.from(msg));
-
-//       console.log(" [x] Sent %s", msg);
-//     });
-//   });
-// }, 10000);
 
 const broker = require("./producer");
 
 setTimeout(() => {
   broker.start().catch(err => console.error(err));
 }, 8000);
+
 // fs.watch("./entry", (event, fileName) => {
 //   console.log(`event type is: ${event} and ${fileName}`);
 
